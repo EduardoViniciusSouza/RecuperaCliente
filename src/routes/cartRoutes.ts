1b6b179983c0d95aab0288b3d1251b63e9bba655
EduardoViniciusSouza <eduardovinicius.macielsouza@gmail.com>
2024-08-15 09:58:10 -0300

    esqueci de salvar o arquivo

--- src/routes/cartRoutes.ts
+++ src/routes/cartRoutes.ts
@@ -1,15 +1,14 @@
 import { Router } from "express";
 import apiKeyMiddleware from "../middlewares/apiKeyMiddleware";
-import { controllerCreateCart, controllerGetActiveCart, controllerRemoveItemFromCart } from "../controllers/cartController";
 
 const cartRouter = Router();
 
 cartRouter.use(apiKeyMiddleware);
 
 cartRouter.get('/inactive/cart'); // todos os carrinhos
-cartRouter.get('/active/cart/:userId', controllerGetActiveCart); // unico carrinho (id)
-cartRouter.post('/cart', controllerCreateCart); // cria carrinho
+cartRouter.get('/active/cart/:userId', ); // unico carrinho (id)
+cartRouter.post('/cart', ); // cria carrinho
 cartRouter.put('/cart/:id'); // altera carrinho por (id)
-cartRouter.delete('/cart/:userId/remove/:productId', controllerRemoveItemFromCart); // deleta carrinho
+cartRouter.delete('/cart/:userId/remove/:productId', ); // deleta carrinho
 
 export default cartRouter;
